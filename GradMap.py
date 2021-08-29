a1cb00cefc5a7d860531e94f0b1611c6711a0b6f
Astrodhan <yashodhan.manerikar@gmail.com>
2021-08-29 20:14:09 +0530

    added better comments

--- GradMap.py
+++ GradMap.py
@@ -23,8 +23,9 @@ from datetime import datetime
 data1='HD59967.vels'                                                            #File name of first data file
 data2='HD76653.vels'
 data3='HD115820.vels'                                                           #Has very few entries
+                                                                                #For now, you have to manually add stars like this.
 
-star=data1
+star=data1                                                                      #Change here to change the star
 data = np.genfromtxt(star)                                                      #Reading the file
 times= data[:,0]                                                                #Reading time from the file
 times=times-times[0]                                                            #Resetting the origin of time to beginning of the observation period
@@ -33,19 +34,20 @@ rv=rv-np.median(rv)                                                             
 error=data[:,2]                                                                 #Reading error values off the file
 
 #SECTION 2: CALCULATING PARAMETERS
-diff=np.zeros(len(times))                                                       #List for times periods between two successive measurements
+diff=np.zeros(len(times))                                                       #diff is the array which contains [t(n+1)-t(n)] entries
 for i in range(len(times)-1):
     diff[i]=times[i+1]-times[i]
     
-f_max=1/max(diff)                                                               #Maximum frequency that could be safely extracted
-f_min=1/(max(times)-min(times))                                                 #Minimum frequency that could be certainly extracted
+f_max=1/max(diff)                                                               #Maximum frequency that could be safely extracted, I have chosen max(diff) instead of min(diff), it should not make much difference in regularly spaced data
+f_min=1/(max(times)-min(times))                                                 #Minimum frequency that could be certainly extracted, max(times)-min(times) is the total time-window of observations.
 res = 500                                                                       #The resolution/least count of frequency
-farray = np.linspace(f_min,f_max,res)
 farray=np.linspace(f_min,f_max,res)                                             #The freq range over which we will calculate the periodogram
 
-prim=pyPeriod.Gls((times,rv))                                                   #This is the original periodogram 
-fapp=prim.powerLevel(0.01)                                                      # this is the false alarm probability pertaining to an error of 1%
+prim=pyPeriod.Gls((times,rv))                                                   #This is the original periodogram, prim for primary.
+fapp=prim.powerLevel(0.01)                                                      # this is the false alarm probability pertaining to an error of 1%; if we have a peak in the periodogram above this level it means that there is only 1% chance of that happening randomly.
+
 ms=1047.94                                                                      # Mass of the star, currently set to 1 Msun in Mjup
+
 #SECTION 3: DEFINING FUNCTIONS
 class pgm:                                                                      #pgm for periodogram
     def _init_(self,f,mp,ms,d):
@@ -55,45 +57,16 @@ class pgm:                                                                      
         self.d=d
     
     def A(ms,mp,f):
-        return 20948.82*f**(1/3)*mp*(mp+ms)**(-2/3)                             #The constant is calculated so that you can input f in 1/day and masses in Mjups
+        return 20948.82*f**(1/3)*mp*(mp+ms)**(-2/3)                             #This gives us the amplitude of RV oscillations in m/s (yashodhan please check) for the combination of particular mass of the planet, sun and frequency. This is calculated through Kepler's third law (actually through newtonian dynamics). The constant 20948.82 is calculated so that you can input f in 1/day and masses in Mjups
     
-    def FT(f,mp,d):                                                             # Resturns the power values of the Fourier Transform of input signal+ added sine
-        rv1=rv+pgm.A(ms,mp,f)*np.sin(2*np.pi*times*f+d)                     #Adding a simulated signal to our RV values, 1047 is solar mass in terms of jupiter masses
-        gls2=pyPeriod.Gls((times,rv1,error),fbeg=f_min,fend=f_max,freq=farray)   #Taking Lomb Scargle Periodogram
+    def FT(f,mp,d):                                                             # Returns the power values of the Fourier Transform of input signal+ added sine. Input: frequency of the simulation, mass of the planet and phase
+        rv1=rv+pgm.A(ms,mp,f)*np.sin(2*np.pi*times*f+d)                         # Adding a simulated signal to our RV values, 1047 is solar mass in terms of jupiter masses
+        gls2=pyPeriod.Gls((times,rv1,error),fbeg=f_min,fend=f_max,freq=farray)  #Taking Lomb Scargle Periodogram
         return gls2.power
-    
-    def fap(f,mp,d,fapv):                                                       # Resturns the power values of the Fourier Transform of input signal+ added sine
-        rv1=rv+pgm.A(ms,mp,f)*np.sin(2*np.pi*times*f+d)                     #Adding a simulated signal to our RV values
-        gls2=pyPeriod.Gls((times,rv1,error),fbeg=f_min,fend=f_max,freq=farray)   #Taking Lomb Scargle Periodogram
-        fapvalues=np.zeros(res)
-        for i in range(0,res):
-            fapvalues[i]=gls2.powerLevel(fapv)                                  #power level gives the power threshold for the given value for FAP
-        return fapvalues
-    
-    def fappowerlevel(f,mp,d,fapv):                                             # Resturns the power values of the Fourier Transform of input signal+ added sine
-        rv1=rv#+pgm.A(1047.94,mp,f)*np.sin(2*np.pi*times*f+d) #Adding a simulated signal to our RV values
-        gls2=pyPeriod.Gls((times,rv1),fbeg=f_min,fend=f_max,freq=farray)
-        return gls2.powerLevel(fapv)
-    
-    def FTu(f,A):                                                               # Power averaged over phase THIS FUNCTION IS A RELIC, NOT UPDATED DON'T USE 
-        power=np.zeros(res)
-        for i in range(100):
-            ph=i*2*np.pi/100
-            rv1=rv+A*np.sin(2*np.pi*times*f+ph)
-            gls3=gls.Gls((times,rv1,error),fbeg=f_min,fend=f_max,freq=farray)
-            power=power+gls3.power
-        return power/100
-
-    def windowf():
-        ones=np.zeros(len(times))
-        for i in range(len(times)):
-            ones[i]=2.0
-        window=pyPeriod.Gls((times,np.zeros(len(times))),fbeg=f_min,fend=f_max,freq=farray)
-        return window.power
 
 
 
-#SECTION 4:  SLIDER PLOT (It is made into a big comment, don't use it together, it might hang your computer)
+#SECTION 4:  SLIDER PLOT (It is made into a big comment, don't use it together with gradmap, it might hang your computer)
 """
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.13,bottom=0.25)
@@ -127,46 +100,47 @@ sldrd.on_changed(update)
 plt.show()
 """
 #SECTION 5: DETECTION LIMITS MAPS
-s1=time.time()
+s1=time.time()                                  # Start time for run-time counting
 zres=36                                         # Resolution in the third axis, so the resolution of the probability. zres=10 would mean detection probability can take values from 0 to 1 in steps of 0.1
 xyres=100                                       # Resolution/number of divisions of the x and y axis, of frequency
 bandwidth=(f_max-f_min)/100                     # This is the half-bandwidth around the input frequency, too wide and you get false detections in the maps 
-mplower=0                                       # Lowest mass for the fake signal
+mplower=0                                       # Lowest mass for the fake signal in jupiter masses
 mpupper=1                                       # Upper limit for the mass for the fake signal
-fakemass=np.linspace(mplower,mpupper,xyres)     # This is the mass space
-fakefreq=np.linspace(f_min,f_max,xyres)         # This is the frequency space
-fakemass, fakefreq=np.meshgrid(fakemass, fakefreq) #Making them both 2D
+fakemass=np.linspace(mplower,mpupper,xyres)     # This is the 1D mass space
+fakefreq=np.linspace(f_min,f_max,xyres)         # This is the 1D frequency space
+fakemass, fakefreq=np.meshgrid(fakemass, fakefreq) #Making them both 2D, I don't fully understand how numpy.meshgrid works.
 rows, cols = (xyres, xyres) 
-prob = [[1 for i in range(cols)] for j in range(rows)] # Detection Probability Matrix
-peakpow=[[0 for i in range(cols)] for j in range(rows)] # Peak power values
+prob = [[1 for i in range(cols)] for j in range(rows)] # Detection Probability Matrix, starting all values with 1.
+peakpow=[[0 for i in range(cols)] for j in range(rows)] # Peak power values, starting it as a null matrix
 amp=[[0 for i in range(cols)] for j in range(rows)]     #Amplitude values
 
 for i in range(xyres):
     for j in range(xyres):
         amp[i][j]=pgm.A(ms,fakemass[j][j],fakefreq[i][i]) #amplitude values calculated here. They are not used in calculation, only to be recorded in data.
         
-def detprob(mp,f):                               # Indices are inputs Calculates the fraction of phase for which the periodogram signal corresponding to the added signal exceeds the False Alarm Probability
+def detprob(mp,f):                              # Indices are inputs. Calculates the fraction of phase for which the periodogram signal corresponding to the added signal exceeds the False Alarm Probability
     fband=[]                                    # fband is the approximate frequncy band around the input frequency
                  
-    powvals=[]                                  # These will be the respective power values for the chosen band
+    powvals=[]                                  # These will be the respective power values for the chosen band, a subset of farray.
     dp=0                                        # dp stands for detection probability 
+    
     for i in range(0,res):
         if farray[i]>f-bandwidth and farray[i]<f+bandwidth:
             fband.append(i)                     # i is appended and not farray[i] because the power array follows the same indexing.
      
     for i in range(zres):
-        powar=pgm.FT(f,mp,i*2*np.pi/zres)
+        powar=pgm.FT(f,mp,i*2*np.pi/zres)       #Incorrect spelling to avoid mix-up
         for j in fband:
             powvals.append(powar[j])
         if max(powvals)>fapp:                   # if the peak of the powervalue exceeds the false alarm probability level then it is a detection
             dp+=1
-    return dp/zres, max(powvals)
+    return dp/zres, max(powvals)                #This gives you 1 single detection probability. For example '0.5'.
 
 def execute():                                                                  #This function is created so you can avoid the main calculation when testing other parts of the code             
     """This function uses while condition and for loop to cover
     the mass-frequency field. While condition stops the calculations when 
-    we have probability=1 for an entire range of frequency, because
-    here on it's the same for all masses all frequencies """
+    we have probability=1 for an entire range of frequency, because from
+    here on it's the same for all masses and all frequencies """
     i=0                                                                 
     while i<xyres:
         for j in range(xyres):
@@ -187,7 +161,7 @@ def writedata(Z,P,X,Y,amp):
     os.mkdir(timestamp)
     os.chdir(timestamp)
     plt.savefig('map.png')
-    f=open('DATA'+timestamp[-6:-1]+'.txt','a')
+    f=open('DATA'+timestamp[-6:-1]+'.txt','a')                                  # This is just to create unique names, to avoid files being over-written.
     f.write('Probability Power PlanetMass Frequency Period RVamplitude')
     f.write('\n')
     roff=2       #round off number of decimals
